5abfe881275ccffe70fce3aa3b8c0b558c27f108
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-05-18 00:27:08 +0300

    Implement CryptoCurrencyCard.

---
+++ src/feature/catalog/Card.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
+
+interface CardProps {
+    style?: StyleProp<ViewStyle> | undefined;
+    children: ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ style, children }) => {
+    return (
+        <View style={[styles.card, style]}>{children}</View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 8,
+        backgroundColor: 'white'
+    }
+})
+
+export default Card;
---
+++ src/feature/catalog/CryptoCurrencyCard.tsx
@@ -0,0 +1,143 @@
+import React from "react"
+import { TouchableOpacity } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
+import { useAppTheme } from "../theme/ThemeContext"
+import Card from "./Card"
+
+type CryptoCurrencyProps = {
+    name: string,
+    symbol: string,
+    logoUrl: string,
+    price: string,
+    change: string,
+    volume: string,
+    marketCap: string
+}
+
+const CryptoCurrencyCard = (props: CryptoCurrencyProps): JSX.Element => {
+    return(
+        <TouchableOpacity style={styles.container}>
+            <CryptoCurrencyHolder {...props} />
+            <CryptoCurrencyPrice {...props} />
+        </TouchableOpacity>
+    )
+}
+
+const CryptoCurrencyHolder = (props: CryptoCurrencyProps): JSX.Element => {
+    const { colors, shapes } = useAppTheme()
+
+    return(
+        <Card style={[styles.holder, { borderRadius: shapes.small, backgroundColor: colors.surface }]}>
+            <CryptoCurrencyLogo {...props} />
+            <CryptoCurrencyDetails {...props} />
+        </Card>
+    )
+}
+
+const CryptoCurrencyLogo = (props: CryptoCurrencyProps): JSX.Element => {
+    const { dimensions, typography } = useAppTheme()
+
+    return(
+        <View style={[styles.logoContainer, { padding: dimensions.contentPadding }]}>
+            <View style={styles.logo}>
+                <Image
+                    source={{ uri: props.logoUrl }}
+                    style={{ width: dimensions.logoSize, height: dimensions.logoSize, margin: dimensions.smallPadding }}
+                />
+                <Text style={[typography.smallLabel]}>{props.symbol}</Text>
+            </View>
+            <Text style={[typography.smallLabel, { fontWeight: 'bold' }]}>{props.name}</Text>
+        </View>
+    )
+}
+
+
+const CryptoCurrencyPrice = (props: CryptoCurrencyProps): JSX.Element => {
+    const { typography } = useAppTheme()
+
+    return(
+        <Text 
+            numberOfLines={1}
+            style={[typography.title, styles.price]}>{props.price}
+        </Text>
+    )
+}
+
+const CryptoCurrencyDetails = (props: CryptoCurrencyProps): JSX.Element => {
+    const { dimensions, typography } = useAppTheme()
+
+    return(
+        <View style={[styles.detailContainer, { padding: dimensions.contentPadding } ]}>
+            <View style={[styles.detailItemValueHolder, { paddingEnd: dimensions.smallPadding } ]}>
+                <Text style={[typography.smallLabel, styles.itemValue, { color: parseFloat(props.change) < 0 ? 'red' : 'green' } ]}>
+                    {parseFloat(props.change) < 0 ? `${props.change}%` : `+${props.change}%` }
+                </Text>
+                <Text style={[typography.smallLabel, styles.itemValue]}>{props.volume}</Text>
+                <Text style={[typography.smallLabel, styles.itemValue]}>{props.marketCap}</Text>
+            </View>
+            <View style={styles.detailItemNameHolder}>
+                <Text style={[typography.smallLabel, styles.itemName]}>24h</Text>
+                <Text style={[typography.smallLabel, styles.itemName]}>Vol</Text>
+                <Text style={[typography.smallLabel, styles.itemName]}>Cap</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    holder: {
+        flexDirection: 'row', /* Main-axis direction */
+        flexWrap: 'nowrap',
+        justifyContent: 'space-between', /* Main-axis: - horizontal */
+        //alignItems: 'center', /* Cross-axis: - vertical */
+        width: '100%'
+    },
+
+    logoContainer: {
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    logo: {
+        flexDirection: 'row'
+    },
+    name: {
+        fontWeight: 'bold'
+    },
+
+    price: {
+        fontWeight: "bold",
+        overflow: "hidden",
+        alignSelf: 'center',
+        position: 'absolute'
+    },
+
+    detailContainer: {
+        flexDirection: 'row'
+    },
+    detailItemNameHolder: {
+        flexDirection: 'column',
+        direction: 'ltr',
+        justifyContent: 'space-evenly'
+    },
+    detailItemValueHolder: {
+        flexDirection: 'column',
+        direction: 'rtl',
+        justifyContent: 'space-evenly'
+    },
+
+    itemName: {
+        fontWeight: "bold"
+    },
+    itemValue: {
+        alignSelf: 'flex-end'
+    }
+})
+
+export default CryptoCurrencyCard

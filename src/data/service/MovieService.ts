3b7ec3cc87a37790bb338374d837578f6fd29c15
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-05-18 00:24:52 +0300

    Remove movie related things.

--- src/data/service/MovieService.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosResponse } from "axios"
-import axiosInstance from "../Network"
-import { Movie, convertToMovieModel } from "../model/Movie"
-
-export let fecthMovies = (): Promise<AxiosResponse<Array<Movie>>> => axiosInstance.get('/discover/movie')
-
-export let moviesConverter = (result: any): Array<Movie> => result.results.map((movieDto: any) => convertToMovieModel(movieDto))

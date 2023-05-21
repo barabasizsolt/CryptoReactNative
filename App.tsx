3afaba5d03d56baa6c1b43f6b4394df199ed46ce
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-05-21 22:08:21 +0300

    Integrate the bottom nav into App.

--- App.tsx
+++ App.tsx
@@ -1,29 +1,12 @@
-import { NavigationContainer } from '@react-navigation/native';
-import ExploreScreen from './src/feature/screen/CryptoCurrencyScreen';
-import { ContentColorProvider } from './src/feature/theme/ContentColorContext';
-import { ThemeProvider, useNavigationTheme } from './src/feature/theme/ThemeContext';
+import { ThemeProvider } from './src/feature/theme/ThemeContext';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemedNavigationContainer } from './src/feature/navigation/AppNavigation';
 
 const App = (): JSX.Element => {
     return(
         <ThemeProvider>
             <ThemedNavigationContainer/>
         </ThemeProvider>
-    )
-}
-
-const ThemedNavigationContainer = () => {
-    const navigationTheme = useNavigationTheme()
-    return (
-        <ContentColorProvider contentColor={navigationTheme.colors.text}>
-            <NavigationContainer theme={navigationTheme}>
-                {/* <AppRootNavigationStacks /> */}
-                <SafeAreaProvider>
-                    <ExploreScreen/>
-                </SafeAreaProvider>
-            </NavigationContainer>
-        </ContentColorProvider>
     )
 }
 

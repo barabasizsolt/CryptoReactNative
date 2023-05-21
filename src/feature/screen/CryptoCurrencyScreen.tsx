bc011e6c94c0a6f9f945a53bc4c7a6f8385a0fc1
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-05-21 22:10:09 +0300

    Simplyfy CryptoScreen with scrollable content.

--- src/feature/screen/CryptoCurrencyScreen.tsx
+++ src/feature/screen/CryptoCurrencyScreen.tsx
@@ -1,18 +1,12 @@
 import { useEffect, useState } from "react";
-import { FlatList, View } from "react-native"
 import { Result, ResultType } from "../../data/Result";
-import LoadingIndicator from "../catalog/LoadingIndicator";
-import { CryptoCurrency } from "../../data/model/CryptoCurrency";
+import { CryptoCurrency } from "../../data/model/crypto/CryptoCurrency";
 import { getCryptoCurrencies } from "../../domain/CryptoCurrencyUseCase";
 import CryptoCurrencyCard from "../catalog/CryptoCurrencyCard";
 import { formatCompactDollarValue, formatDollarValue } from "../../data/util/Converter";
-import { useAppTheme } from "../theme/ThemeContext";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { EdgeToEdgeScrollableContent } from "../catalog/EdgeToEdgeScrollableContent";
 
-const ExploreScreen = (): JSX.Element => {
-
-    const insets = useSafeAreaInsets();
-    const { colors, dimensions } = useAppTheme()
+const CryptoCurrencyScreen = (): JSX.Element => {
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [cryptoCurrencies, setCryptoCurrencies] = useState<CryptoCurrency[]>([])
 
@@ -33,36 +27,25 @@ const ExploreScreen = (): JSX.Element => {
         []
     )
 
-    const header = (): JSX.Element => { return (<View style={{ height: insets.top + dimensions.contentPadding }}/>) }
-    const footer = (): JSX.Element => { return (<View style={{ height: insets.bottom + dimensions.contentPadding }}/>) }
-    const itemSeparator = (): JSX.Element => { return (<View style={{height: dimensions.contentPadding}}/>) }
-
-    if (isLoading) {
-        return(LoadingIndicator())
-    } else {
-        return(
-            <FlatList
-                data={cryptoCurrencies}
-                renderItem={ ({ item }) => 
-                    <CryptoCurrencyCard
-                        name={ item.name }
-                        symbol={ item.symbol }
-                        logoUrl={ item.iconUrl }
-                        price={ formatDollarValue(item.price) }
-                        change={ item.change }
-                        volume={ formatCompactDollarValue(item.volume) }
-                        marketCap={ formatCompactDollarValue(item.marketCap) }
-                    />
-                }
-                ListHeaderComponent={ header }
-                ListFooterComponent={ footer }
-                numColumns={ 1 }
-                keyExtractor={ (item) => item.uuid }
-                contentContainerStyle={ { paddingHorizontal: dimensions.contentPadding, backgroundColor: colors.background } } 
-                ItemSeparatorComponent={ itemSeparator }    
-            />
-        )
-    }
+    return(
+        <EdgeToEdgeScrollableContent
+        isLoading={ isLoading }
+        listItems={ cryptoCurrencies } 
+        renderItem={ ({ item }) => {
+            const cryptoItem = item as CryptoCurrency;
+            return (
+                <CryptoCurrencyCard
+                    name={ cryptoItem.name }
+                    symbol={ cryptoItem.symbol }
+                    logoUrl={ cryptoItem.iconUrl }
+                    price={ formatDollarValue(cryptoItem.price) }
+                    change={ cryptoItem.change}
+                    volume={ formatCompactDollarValue(cryptoItem.volume) }
+                    marketCap={ formatCompactDollarValue(cryptoItem.marketCap) }/>
+                )}
+            }
+        />
+    )
 }
 
-export default ExploreScreen
+export default CryptoCurrencyScreen

1ff48ba25afe912e7ebcf92f3bbc84afd568e08d
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-06-06 10:54:03 +0300

    Implement ErrorContent.

---
+++ src/feature/catalog/ErrorContent.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, View } from 'react-native';
+import { useAppTheme } from '../theme/ThemeContext';
+import { TranslatedText } from './TranslatedText';
+import { PrimaryButton } from './PrimaryButton';
+
+type ErrorContentProps = {
+  onPress: () => void;
+};
+
+const ErrorContent = (props: ErrorContentProps): JSX.Element => {
+  const { colors, typography, dimensions } = useAppTheme();
+
+  return (
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <TranslatedText style={[typography.title]} textKey="error_title" />
+      <View
+        style={[
+          styles.refreshButton,
+          {
+            paddingHorizontal: dimensions.screenPadding * 2,
+            paddingTop: dimensions.contentPadding,
+          },
+        ]}>
+        <PrimaryButton textKey="try_again" onPress={props.onPress} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  refreshButton: {
+    width: '100%',
+  },
+});
+
+export default ErrorContent;

fbe15d2387302fd77f5eed722ab6716b13fe008a
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-06-06 10:52:33 +0300

    Change cache controll to 'no-cache'.

--- src/data/Network.tsx
+++ src/data/Network.tsx
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 const BASE_URL: string = 'https://api.coingecko.com/api/v3';
 
-const axiosInstance = axios.create({ baseURL: BASE_URL });
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Cache-Control': 'no-cache',
+    Pragma: 'no-cache',
+    Expires: '0',
+  },
+});
 
 export default axiosInstance;

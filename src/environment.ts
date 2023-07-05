9287e331326c3610bb83c1247de8dccde909874c
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-07-05 10:26:12 +0300

    Add the firebase related keys to Environment.

--- src/environment.ts
+++ src/environment.ts
@@ -2,4 +2,7 @@ export const Environment = {
   coinRankingUrl: process.env.COINRANKING_URL,
   coinGekkoUrl: process.env.COINGEKKO_URL,
   accessToken: process.env.ACCESS_TOKEN,
+  iosApiKey: process.env.FIREBASE_IOS_API_KEY,
+  iosAppId: process.env.FIREBASE_IOS_APP_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 };

799ca9180d7dc8017785d532cdb4a3bb8376c98d
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-07-11 13:37:03 +0300

    Refactor BackButton using Pressable.

--- src/feature/components/catalog/BackButton.tsx
+++ src/feature/components/catalog/BackButton.tsx
@@ -1,6 +1,12 @@
 import { ReactElement } from 'react';
-import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
-import { AnimatedPressable } from '../touch/AnimatedPressable';
+import {
+  Platform,
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from 'react-native';
 import { useAppTheme } from '../../theme/ThemeContext';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -9,34 +15,50 @@ type BackButtonProps = {
   onBackPress: () => void;
 };
 
+/* TODO: Refactor it */
 const BackButton = (props: BackButtonProps): ReactElement => {
   const { colors, dimensions } = useAppTheme();
   return (
-    <AnimatedPressable
-      android_ripple={{ color: colors.rippleColor, borderless: false }}
-      overlayViewStyle={[
+    <View
+      style={[
         props.style,
         styles.container,
-        {
-          width: dimensions.touchTargetSize,
-          height: dimensions.touchTargetSize,
-          borderRadius: dimensions.touchTargetSize / 2,
-          borderWidth: 2,
-          borderColor: 'white',
-          backgroundColor: colors.primary,
-        },
-      ]}
-      onPress={props.onBackPress}>
-      <MaterialCommunityIcons name="chevron-left" color="white" size={30} />
-    </AnimatedPressable>
+        { borderRadius: dimensions.touchTargetSize / 2 },
+      ]}>
+      <Pressable
+        android_ripple={{ color: colors.rippleColor }}
+        style={({ pressed }) => [
+          {
+            width: dimensions.touchTargetSize,
+            height: dimensions.touchTargetSize,
+            borderRadius: dimensions.touchTargetSize / 2,
+            borderWidth: 1,
+            borderColor: colors.onBackground,
+            backgroundColor:
+              Platform.OS === 'ios'
+                ? pressed
+                  ? colors.rippleColor
+                  : colors.background
+                : colors.background,
+
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        ]}
+        onPress={props.onBackPress}>
+        <MaterialCommunityIcons
+          name="chevron-left"
+          color={colors.onBackground}
+          size={30}
+        />
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    overflow: 'hidden',
     position: 'absolute',
   },
 });

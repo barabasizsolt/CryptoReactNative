6441ed25bb8d403ed2e9724735e4d64c248276c6
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-07-12 09:57:34 +0300

    Refactor PrimaryButton.

--- src/feature/components/catalog/PrimaryButton.tsx
+++ src/feature/components/catalog/PrimaryButton.tsx
@@ -5,9 +5,11 @@ import {
   Platform,
   StyleProp,
   TextStyle,
+  ActivityIndicator,
+  View,
 } from 'react-native';
 import { useAppTheme } from '../../theme/ThemeContext';
-import { ReactElement } from 'react';
+import { ReactElement, ReactNode } from 'react';
 import { TranslatedText } from './TranslatedText';
 
 interface PrimaryButtonProps {
@@ -17,39 +19,95 @@ interface PrimaryButtonProps {
   onBackgroundTextColor?: ColorValue;
   borderRadius?: number;
   isEnabled: boolean;
+  isLoading?: boolean;
   textStyle?: StyleProp<TextStyle>;
 }
 
 export const PrimaryButton = (props: PrimaryButtonProps): ReactElement => {
-  const { colors, typography, shapes } = useAppTheme();
   return (
-    <Pressable
-      onPress={props.isEnabled ? props.onPress : void 0}
-      android_ripple={{ color: colors.rippleColor }}
-      style={({ pressed }) => [
-        styles.holder,
-        {
-          backgroundColor: props.isEnabled
-            ? Platform.OS === 'ios' && pressed
-              ? colors.rippleColor
-              : props.backgroundColor ?? colors.primary
-            : colors.rippleColor,
-          borderRadius: props.borderRadius ?? shapes.small,
-        },
-      ]}>
-      <TranslatedText
+    <ButtonWrapper
+      isEnabled={props.isEnabled}
+      borderRadius={props.borderRadius}
+      backgroundColor={props.backgroundColor}
+      onPress={props.onPress}>
+      <ButtonText
+        isLoading={props.isLoading}
         textKey={props.textKey}
-        style={[
-          typography.inputLabel,
-          props.textStyle,
-          {
-            color: props.isEnabled
-              ? props.onBackgroundTextColor ?? colors.onPrimary
-              : 'black',
-          },
-        ]}
+        textStyle={props.textStyle}
+        isEnabled={props.isEnabled}
+        onBackgroundTextColor={props.onBackgroundTextColor}
       />
-    </Pressable>
+    </ButtonWrapper>
+  );
+};
+
+type ButtonWrapperProps = {
+  isEnabled: boolean;
+  borderRadius?: number;
+  backgroundColor?: ColorValue;
+  onPress: () => void;
+  children: ReactNode;
+};
+
+const ButtonWrapper = (props: ButtonWrapperProps): ReactElement => {
+  const { colors, shapes } = useAppTheme();
+
+  return (
+    <View
+      style={{
+        borderRadius: props.borderRadius ?? shapes.large,
+        overflow: 'hidden',
+      }}>
+      <Pressable
+        onPress={props.isEnabled ? props.onPress : void 0}
+        android_ripple={{ color: colors.rippleColor }}
+        style={({ pressed }) => [
+          styles.holder,
+          {
+            backgroundColor: props.isEnabled
+              ? Platform.OS === 'ios' && pressed
+                ? colors.rippleColor
+                : props.backgroundColor ?? colors.primary
+              : colors.disabled,
+            borderRadius: props.borderRadius ?? shapes.large,
+          },
+        ]}>
+        {props.children}
+      </Pressable>
+    </View>
+  );
+};
+
+type ButtonTextProps = {
+  isLoading: boolean | undefined;
+  textKey: string;
+  textStyle?: StyleProp<TextStyle>;
+  isEnabled: boolean;
+  onBackgroundTextColor?: ColorValue;
+};
+
+const ButtonText = (props: ButtonTextProps): ReactElement => {
+  const { colors, typography } = useAppTheme();
+
+  return (
+    <>
+      {props.isLoading ? (
+        <ActivityIndicator size="large" color="white" />
+      ) : (
+        <TranslatedText
+          textKey={props.textKey}
+          style={[
+            typography.inputLabel,
+            props.textStyle,
+            {
+              color: props.isEnabled
+                ? props.onBackgroundTextColor ?? colors.onPrimary
+                : colors.onBackground,
+            },
+          ]}
+        />
+      )}
+    </>
   );
 };
 

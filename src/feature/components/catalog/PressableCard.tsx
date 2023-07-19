6d0c377cf16bb7b0df38bdccaa2bd4aaa45c0260
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-07-19 10:23:46 +0300

    Wrap PressableCard into a View to support styling.

--- src/feature/components/catalog/PressableCard.tsx
+++ src/feature/components/catalog/PressableCard.tsx
@@ -19,26 +19,27 @@ export const PressableCard = (props: PressableCardProps): ReactElement => {
   const { colors, shapes } = useAppTheme();
 
   return (
-    <View style={[styles.card, { borderRadius: shapes.medium }]}>
-      <Pressable
-        style={({ pressed }) => [
-          props.style,
-          {
-            borderWidth: 1,
-            borderRadius: shapes.medium,
-            borderColor: colors.disabled,
-            backgroundColor:
-              Platform.OS === 'ios'
-                ? pressed
-                  ? colors.rippleColor
-                  : colors.background
-                : colors.background,
-          },
-        ]}
-        android_ripple={{ color: colors.rippleColor }}
-        onPress={props.onItemClick}>
-        {props.children}
-      </Pressable>
+    <View style={props.style}>
+      <View style={[styles.card, { borderRadius: shapes.medium }]}>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              borderWidth: 1,
+              borderRadius: shapes.medium,
+              borderColor: colors.disabled,
+              backgroundColor:
+                Platform.OS === 'ios'
+                  ? pressed
+                    ? colors.rippleColor
+                    : colors.background
+                  : colors.background,
+            },
+          ]}
+          android_ripple={{ color: colors.rippleColor }}
+          onPress={props.onItemClick}>
+          {props.children}
+        </Pressable>
+      </View>
     </View>
   );
 };
